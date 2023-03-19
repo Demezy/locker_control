daa25164b28528cc1158aa508888966b18c41565
Demezy <38487319+Demezy@users.noreply.github.com>
2023-03-19 18:24:27 +0300

    feat: add provider for LockerRepository and its state

---
+++ lib/data/locker_locks_provider.dart
@@ -0,0 +1,10 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'locker_locks_repository.dart';
+
+final lockerRepositoryProvider =
+    StateProvider<LockerRepository>((ref) => LockerRepository());
+final lockerStateProvider = StreamProvider.autoDispose<Locks>((ref) {
+  final lockerRepository = ref.watch(lockerRepositoryProvider);
+  return lockerRepository.stream;
+});
