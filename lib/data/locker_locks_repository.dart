110b69fb8b62577436cfd6d5adb75246608c9780
Demezy <38487319+Demezy@users.noreply.github.com>
2023-03-19 18:23:58 +0300

    feat: implement LockerRepository api

--- lib/data/locker_locks_repository.dart
+++ lib/data/locker_locks_repository.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
 
@@ -8,6 +10,19 @@ typedef Locks = List<LockModel>;
 
 class LockerRepository {
   final _dio = GetIt.I.get<Dio>();
+  // TODO: move to Hive
+  Locks _locksState = [];
+  final _lockerController = StreamController<Locks>();
+
+  LockerRepository() {
+    retrieveLocks();
+  }
+
+  void dispose() {
+    _lockerController.close();
+  }
+
+  Stream<Locks> get stream => _lockerController.stream;
 
   Future<Locks> retrieveLocks() async {
     late final Locks locks;
@@ -17,9 +32,21 @@ class LockerRepository {
       final response = await _dio.get(apiUrl);
       locks = ApiResponseModel.fromJson(response.data).locks;
     } on DioError catch (e) {
-      // TODO: add some logic here 
+      // TODO: add some logic here
       throw UnimplementedError('Api server exception unhandled');
     }
-    return locks;
+    _locksState = locks.toList();
+    _lockerController.add(_locksState);
+    return _locksState;
+  }
+
+  Future<void> toggleLockStateById(int lockId, bool state) async {
+    final lockIndex = _locksState.indexWhere((lock) => lock.id == lockId);
+    _locksState[lockIndex] = _locksState[lockIndex].copyWith(isLock: state);
+    _lockerController.add(_locksState);
+  }
+
+  Future<void> uploadLocks(Locks locks) async {
+    throw UnimplementedError('Data uploading is not implemented');
   }
 }
